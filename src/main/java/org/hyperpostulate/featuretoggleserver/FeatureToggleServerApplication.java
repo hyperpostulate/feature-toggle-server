cd0b8d421950b1bcc5c57b0482561a45b2c73c26
hyperpostulate <mesutormanli@gmail.com>
2023-06-22 01:00:01 +0300

    initial commit

---
+++ src/main/java/org/hyperpostulate/featuretoggleserver/FeatureToggleServerApplication.java
@@ -0,0 +1,13 @@
+package org.hyperpostulate.featuretoggleserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureToggleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureToggleServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/hyperpostulate/featuretoggleserver/FeatureToggleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hyperpostulate.featuretoggleserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureToggleServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
